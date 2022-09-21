fe95cad0b695900b6f68f74f0a992a56ba6b6c1e
fsx950223 <fsx950223@gmail.com>
2022-09-21 02:27:42 +0000

    add benchmark script

--- benchmarks/benchmark_flash_attention.py
+++ benchmarks/benchmark_flash_attention.py
@@ -10,6 +10,9 @@ from benchmarks.utils import benchmark_all, benchmark_forward, benchmark_backwar
 from flash_attn.bert_padding import unpad_input, pad_input
 from flash_attn.flash_attn_interface import flash_attn_unpadded_qkvpacked_func
 
+import xlwt
+
+DEVICE_TFLOPS = 312
 
 def attention_ref(qkv, attn_mask, dropout_p, upcast=False, causal=False):
     """
@@ -38,33 +41,69 @@ def attention_ref(qkv, attn_mask, dropout_p, upcast=False, causal=False):
 
 torch.manual_seed(0)
 repeats = 30
-batch_size = 64
-nheads = 16
-seqlen = 1024
-n = 1024
-d = n // nheads
-dropout_p = 0.1
+
+dropout_p = 0.0
 causal = False
 dtype = torch.float16
 device = 'cuda'
 
-x = torch.randn(batch_size, seqlen, n, device='cuda', dtype=dtype, requires_grad=True)
-Wqkv = torch.nn.Linear(nheads * d, 3 * nheads * d, device=device, dtype=dtype)
+batch_sizes = [1, 2, 8, 32, 64]
+num_heads = [16, 12]
+seqlens = [1, 2, 16, 32, 128, 256]
+head_dims = [64]
+configs = []
+for batch_size in batch_sizes:
+    for num_head in num_heads:
+        for seqlen in seqlens:
+            for head_dim in head_dims:
+                configs.append([batch_size, num_head, seqlen, head_dim])
 
-lengths = torch.randint(seqlen - 20, seqlen, (batch_size, 1), device='cuda')
-attention_mask_bool = repeat(torch.arange(seqlen, device='cuda'), 's -> b s', b=batch_size) < lengths
-attention_mask = torch.zeros(batch_size, seqlen, device='cuda', dtype=dtype)
-attention_mask[~attention_mask_bool] = -10000.0
-attention_mask = rearrange(attention_mask, 'b s -> b 1 1 s')
+workbook = xlwt.Workbook(encoding= 'ascii')
+worksheet = workbook.add_sheet("benchmark")
 
-x_unpad, indices, cu_seqlens, max_seqlen_in_batch = unpad_input(x, attention_mask_bool)
-qkv_unpad = rearrange(Wqkv(x_unpad), 'nnz (t h d) -> nnz t h d', t=3,
-                      h=nheads).detach().requires_grad_()
-qkv = rearrange(Wqkv(x), 'b s (t h d) -> b s t h d', t=3, h=nheads).detach().requires_grad_()
+worksheet.write(0,0, "batch size")
+worksheet.write(0,1, "num heads")
+worksheet.write(0,2, "num heads * batch size")
+worksheet.write(0,3, "seqlen")
+worksheet.write(0,4, "head dim")
+worksheet.write(0,5, "run time on A100/ms")
+worksheet.write(0,6, "tflops")
+worksheet.write(0,7, "util/%")
 
-fn = lambda qkv_unpad: flash_attn_unpadded_qkvpacked_func(
-    qkv_unpad, cu_seqlens, max_seqlen_in_batch, dropout_p, causal=causal
-)
-benchmark_all(fn, qkv_unpad, repeats=repeats, desc='FlashAttention')
-fn = lambda qkv: attention_ref(qkv, attention_mask_bool, dropout_p, causal=causal)
-benchmark_all(fn, qkv, repeats=repeats, desc='PyTorch Standard Attention')
+for i, (batch_size, nheads, seqlen, d) in enumerate(configs):
+    index = i + 1
+    n = d * nheads  
+    flop = (seqlen * seqlen * d * 2 + seqlen * seqlen * d * 2) * batch_size * nheads
+    x = torch.randn(batch_size, seqlen, n, device='cuda', dtype=dtype, requires_grad=True)
+    Wqkv = torch.nn.Linear(nheads * d, 3 * nheads * d, device=device, dtype=dtype)
+
+    lengths = torch.randint(seqlen - 20, seqlen, (batch_size, 1), device='cuda')
+    attention_mask_bool = repeat(torch.arange(seqlen, device='cuda'), 's -> b s', b=batch_size) < lengths
+    attention_mask = torch.zeros(batch_size, seqlen, device='cuda', dtype=dtype)
+    attention_mask[~attention_mask_bool] = -10000.0
+    attention_mask = rearrange(attention_mask, 'b s -> b 1 1 s')
+
+    x_unpad, indices, cu_seqlens, max_seqlen_in_batch = unpad_input(x, attention_mask_bool)
+    qkv_unpad = rearrange(Wqkv(x_unpad), 'nnz (t h d) -> nnz t h d', t=3,
+                        h=nheads).detach().requires_grad_()
+    qkv = rearrange(Wqkv(x), 'b s (t h d) -> b s t h d', t=3, h=nheads).detach().requires_grad_()
+
+    fn = lambda qkv_unpad: flash_attn_unpadded_qkvpacked_func(
+        qkv_unpad, cu_seqlens, max_seqlen_in_batch, dropout_p, causal=causal
+    )
+
+    _, ave_time = benchmark_forward(fn, qkv_unpad, repeats=repeats, desc='FlashAttention')
+    ave_time = ave_time.mean * 1000
+    tflops = flop / 1e9 / ave_time
+    worksheet.write(index, 0, batch_size)
+    worksheet.write(index, 1, nheads)
+    worksheet.write(index, 2, nheads * batch_size)
+    worksheet.write(index, 3, seqlen)
+    worksheet.write(index, 4, d)
+    worksheet.write(index, 5, ave_time)
+    worksheet.write(index, 6, tflops)
+    worksheet.write(index, 7, 100 * float(tflops) / DEVICE_TFLOPS)
+
+workbook.save('summary.xls')
+# fn = lambda qkv: attention_ref(qkv, attention_mask_bool, dropout_p, causal=causal)
+# benchmark_all(fn, qkv, repeats=repeats, desc='PyTorch Standard Attention')
